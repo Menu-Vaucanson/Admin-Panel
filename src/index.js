ec51b15956c612e655b02fbfee50c982e2f26602
unel <enee.prog@gmail.com>
2022-09-16 08:51:01 +0200

    Co-authored-by: Wiwok <Wiwok@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { useState } from 'react';
 import './Stylesheets/index.css';
 
 import Main from './Components/Main';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+function Index() {
+	const [index, setIndex] = useState(
+		<div className='Index'>
+			<div className='entrée le mot de passe'
+		</div>
+		// if (TestToken(window.localStorage.getItem(token))) {
+		// 	return (
+		// 		<BrowserRouter>
+		// 			<Main />
+		// 		</BrowserRouter >
+		// 	)
+
+		// }
+
+	);
+	return index;
+}
+
 root.render(
-	<BrowserRouter>
-		<Main />
-	</BrowserRouter >
+	<Index />
 );
+
+function TestToken(token) {
+	return true
+}

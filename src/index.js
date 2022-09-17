890b37136f17ad4d548700f8a2bcc8747bfbe286
unel <enee.prog@gmail.com>
2022-09-17 19:31:58 +0200

    create connect page

--- src/index.js
+++ src/index.js
@@ -2,26 +2,44 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { useState } from 'react';
+import Logo from './Assets/Logo.png';
 import './Stylesheets/index.css';
-
 import Main from './Components/Main';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// if (document.getElementById('nameOfSession').value === '') {
+// 	StateEdition(setState, 'Tu dois indiquer un nom', 'error');
+// }
+
 function Index() {
 	const [index, setIndex] = useState(
 		<div className='Index'>
-			<div className='entrée le mot de passe'
-		</div>
-		// if (TestToken(window.localStorage.getItem(token))) {
-		// 	return (
-		// 		<BrowserRouter>
-		// 			<Main />
-		// 		</BrowserRouter >
-		// 	)
-
-		// }
-
+			<div className='passwordText'>Bienvenue à l'administration <br /> du Menu Vaucanson</div>
+			<img className='PasswordLogo' src={Logo} alt='Logo' />
+			<div className='passwordBox'>
+				<input type='password' className='FormInput' placeholder='Mot de Passe' id='password' />
+				<div className='conectButon' onClick={validatePassword}>Connexion</div>
+			</div>
+		</div >
 	);
+	function validatePassword() {
+		const token = document.getElementById('password').value;
+		if (TestToken(token)) {
+			setIndex(
+				<BrowserRouter>
+					<Main />
+				</BrowserRouter >
+			)
+		}
+	}
+	if (window.localStorage.getItem('token') && TestToken(window.localStorage.getItem('token'))) {
+		setIndex(
+			<BrowserRouter>
+				<Main />
+			</BrowserRouter >
+		)
+	}
 	return index;
 }
 

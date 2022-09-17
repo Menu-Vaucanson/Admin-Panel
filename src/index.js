7b1b97a201d919509f90589d1cf122c8f4bbfed4
Wiwok <Emil.dechenaud@gmail.com>
2022-09-17 22:00:28 +0200

    Added login page

--- src/index.js
+++ src/index.js
@@ -2,51 +2,80 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { useState } from 'react';
+import axios from 'axios';
+
 import Logo from './Assets/Logo.png';
 import './Stylesheets/index.css';
 import Main from './Components/Main';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// if (document.getElementById('nameOfSession').value === '') {
-// 	StateEdition(setState, 'Tu dois indiquer un nom', 'error');
-// }
+const url = 'http://menuvox.fr:8081';
 
-function Index() {
-	const [index, setIndex] = useState(
-		<div className='Index'>
-			<div className='passwordText'>Bienvenue à l'administration <br /> du Menu Vaucanson</div>
-			<img className='PasswordLogo' src={Logo} alt='Logo' />
-			<div className='passwordBox'>
-				<input onKeyPress={(e) => e.key === 'Enter' && validatePassword()} type='password' className='FormInput' placeholder='Mot de Passe' id='password' />
-				<div className='conectButon' onClick={validatePassword}>Connexion</div>
-			</div>
-		</div >
-	);
-	function validatePassword() {
-		const token = document.getElementById('password').value;
-		if (TestToken(token)) {
-			setIndex(
+function Send(token) {
+	return new Promise(resolve => {
+		axios.post(`${url}/login`, { 'jwt': token }).catch(err => {
+			console.log(err);
+			resolve(null);
+		}).then(response => {
+			if (typeof response == 'undefined') {
+				resolve(null);
+			} else {
+				console.log(response.data);
+				resolve(true);
+			}
+		});
+	});
+}
+
+function LoginPage() {
+	const [ButtonText, setButtonText] = useState('Se connecter');
+
+	const oldToken = JSON.parse(window.localStorage.getItem('jwt'));
+
+	Send(oldToken).then(res => {
+		if (res) {
+			root.render(
 				<BrowserRouter>
 					<Main />
 				</BrowserRouter >
-			)
+			);
 		}
+	})
+
+	function click() {
+		const token = document.getElementById('password').value;
+		if (token === '') return;
+		setButtonText('Connection...');
+		Send(token).then(res => {
+			if (res === true) {
+				window.localStorage.setItem('jwt', JSON.stringify(token));
+				root.render(
+					<BrowserRouter>
+						<Main />
+					</BrowserRouter >
+				)
+			} else {
+				setButtonText('Mauvais mot de passe');
+			}
+			setTimeout(() => {
+				setButtonText('Connection');
+			}, 1000);
+		})
 	}
-	if (window.localStorage.getItem('token') && TestToken(window.localStorage.getItem('token'))) {
-		setIndex(
-			<BrowserRouter>
-				<Main />
-			</BrowserRouter >
-		)
-	}
-	return index;
+
+	return (
+		<div className='LoginPage'>
+			<div className='LoginTitle'>Bienvenue a l'administration de Menu Vaucanson</div>
+			<img className='LoginLogo' src={Logo} alt='Logo' />
+			<input onKeyUp={(e) => {
+				if (e.key === 'Enter') click();
+			}} type="password" placeholder='Mot de passe' id='password'></input>
+			<div className='LoginButton' onClick={click}>{ButtonText}</div>
+		</div>
+	);
 }
 
 root.render(
-	<Index />
-);
-
-function TestToken(token) {
-	return true
-}
+	<LoginPage />
+)

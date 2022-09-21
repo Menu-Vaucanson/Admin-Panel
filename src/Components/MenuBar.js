1b5fa5dd2faf41d53e58abe991fa71d99b2e7049
Wiwok <Emil.dechenaud@gmail.com>
2022-09-21 14:38:40 +0200

    Added logout option

--- src/Components/MenuBar.js
+++ src/Components/MenuBar.js
@@ -1,10 +1,17 @@
 import { Link } from 'react-router-dom';
 
 import Logo from '../Assets/Logo.png';
+import Logout from '../Assets/logout.svg';
 
 function MenuBar() {
+	function click() {
+		window.localStorage.clear();
+		window.location.href = '/';
+	}
+
 	return (
 		<div className="MenuBar">
+			<img src={Logout} alt='Déconnexion' onClick={click} className='LogoutButton' />
 			<Link to='/' className='MenuBarTitle'>Menu Vaucanson Admin</Link>
 			<img className='MenuLogo' src={Logo} alt='Logo' />
 		</div >

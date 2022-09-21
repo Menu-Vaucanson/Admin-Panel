dd29666404dd06fb95f5aa03f185ed4e10669d0d
Wiwok <Emil.dechenaud@gmail.com>
2022-09-21 23:48:40 +0200

    Re-update svg logo

--- src/Components/MenuBar.js
+++ src/Components/MenuBar.js
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 
-import Logo from '../Assets/Logo.png';
+import Logo from '../Assets/Logo.svg';
 import Logout from '../Assets/logout.svg';
 
 function MenuBar() {

fb46519fa115c66a565a047b3e79b43c3453e2ee
unel <enee.prog@gmail.com>
2022-09-25 15:10:24 +0200

    add timing in refresh buton

--- src/Components/Informations/RefreshComp.js
+++ src/Components/Informations/RefreshComp.js
@@ -1,7 +1,14 @@
 import Refresh from '../../Assets/arrow.svg';
-
+import { useState } from 'react';
 function stopRefreshAnimation() {
 	document.getElementById('RefreshButton').className = 'refreshIcon';
+	document.getElementById('ping').className = 'ping Animate';
+	sleep(3000, () => (document.getElementById('ping').className = 'ping stopAnimate'))
+}
+
+async function sleep(milliseconds, callback) {
+	await new Promise(resolve => setTimeout(resolve, milliseconds));
+	callback();
 }
 
 function startRefreshAnimation() {
@@ -9,17 +16,35 @@ function startRefreshAnimation() {
 }
 
 function RefreshComp({ callback }) {
+	const [ping, setPing] = useState(
+		<div className='ping' id='ping'>
+			<div className='pingText'>fait en 0 ms</div>
+		</div>
+	);
 
 	function click() {
+		const timestart = new Date();
 		startRefreshAnimation();
 		callback().then(() => {
 			stopRefreshAnimation();
+			printPing(timestart, new Date(), setPing);
 		})
 	}
 
 	return (
-		<img src={Refresh} alt='Rafraichir' className='refreshIcon' onClick={click} id='RefreshButton' />
+		<div>
+			<img src={Refresh} alt='Rafraichir' className='refreshIcon' onClick={click} id='RefreshButton' />
+			{ping}
+		</div>
 	);
+}
+
+
+function printPing(time1, time2, setPing) {
+	const ping = new Date(time2).getTime() - new Date(time1).getTime();
+	setPing(
+		<div className='ping' id='ping'><div className='pingText'>fait en {ping} ms</div></div>
+	)
 }
 
 export { stopRefreshAnimation, startRefreshAnimation };
